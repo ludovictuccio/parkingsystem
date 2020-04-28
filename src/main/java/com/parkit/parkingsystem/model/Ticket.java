ea43ac8060a26c8ba368e5f18a8b3fcca4c18b67
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2020-04-28 14:29:20 +0200

    Method to round the price to the hundredth added
    Math.round added on Ticket.class

--- src/main/java/com/parkit/parkingsystem/model/Ticket.java
+++ src/main/java/com/parkit/parkingsystem/model/Ticket.java
@@ -40,7 +40,7 @@ public class Ticket {
     }
 
     public void setPrice(double price) {
-	this.price = price;
+	this.price = Math.round(price * 100.0) / 100.0;
     }
 
     public LocalDateTime getInTime() {
