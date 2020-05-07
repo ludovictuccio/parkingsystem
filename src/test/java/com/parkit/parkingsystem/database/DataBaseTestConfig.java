e5df30c51cbd166bb6c02be1e3e5aaadcb7b5e60
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2020-05-07 15:22:53 +0200

    ParkingService integration test
    Incoming and exiting verify; DataBaseTestConfig changes

--- src/test/java/com/parkit/parkingsystem/database/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/database/DataBaseTestConfig.java
@@ -1,10 +1,15 @@
 package com.parkit.parkingsystem.database;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,21 +18,34 @@ import com.parkit.parkingsystem.config.DataBaseConfig;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
-    private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
+    private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+    private static final String SQL_CREDENTIALS_SECURITY = "src/main/resources/SQL_credentials_security.properties";
+    private String url;
+    private String userName;
+    private String password;
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-	logger.info("Create DB connection");
+	logger.debug("Create DB connection");
 	Class.forName("com.mysql.cj.jdbc.Driver");
-	return DriverManager.getConnection(
-		"jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris",
-		"root", "rootroot");
+	try (InputStream inputStream = new FileInputStream(SQL_CREDENTIALS_SECURITY)) {
+	    Properties properties = new Properties();
+	    properties.load(inputStream);
+	    url = properties.getProperty("url");
+	    userName = properties.getProperty("userName");
+	    password = properties.getProperty("password");
+	} catch (FileNotFoundException fnf) {
+	    logger.error("File not found. Please verify sql_credentials_file access root.", fnf);
+	} catch (IOException ioex) {
+	    logger.error("Error during SQL connection. Please check the contents of the file.", ioex);
+	}
+	return DriverManager.getConnection(url, userName, password);
     }
 
     public void closeConnection(Connection con) {
 	if (con != null) {
 	    try {
 		con.close();
-		logger.info("Closing DB connection");
+		logger.debug("Closing DB connection");
 	    } catch (SQLException e) {
 		logger.error("Error while closing connection", e);
 	    }
@@ -38,7 +56,7 @@ public class DataBaseTestConfig extends DataBaseConfig {
 	if (ps != null) {
 	    try {
 		ps.close();
-		logger.info("Closing Prepared Statement");
+		logger.debug("Closing Prepared Statement");
 	    } catch (SQLException e) {
 		logger.error("Error while closing prepared statement", e);
 	    }
@@ -49,7 +67,7 @@ public class DataBaseTestConfig extends DataBaseConfig {
 	if (rs != null) {
 	    try {
 		rs.close();
-		logger.info("Closing Result Set");
+		logger.debug("Closing Result Set");
 	    } catch (SQLException e) {
 		logger.error("Error while closing result set", e);
 	    }
--- src/test/java/com/parkit/parkingsystem/integration/ParkingServiceIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingServiceIT.java
@@ -1,11 +1,27 @@
 package com.parkit.parkingsystem.integration;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 
@@ -13,7 +29,12 @@ import com.parkit.parkingsystem.util.InputReaderUtil;
 public class ParkingServiceIT {
 
     private static ParkingService parkingService;
+    private Ticket ticket;
+    private ParkingSpot parkingSpot;
+    private LocalDateTime inTime;
 
+    @Mock
+    private FareCalculatorService fareCalculatorService;
     @Mock
     private static InputReaderUtil inputReaderUtil;
     @Mock
@@ -21,49 +42,53 @@ public class ParkingServiceIT {
     @Mock
     private static TicketDAO ticketDAO;
 
-//    @BeforeEach
-//    private void setUpPerTest() {
-//	try {
-//	    when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-//
-//	    ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
-//	    Ticket ticket = new Ticket();
-//	    ticket.setInTime(LocalDateTime.of(2020, 10, 12, 15, 30));
-//	    ticket.setParkingSpot(parkingSpot);
-//	    ticket.setVehicleRegNumber("ABCDEF");
-//	    when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-//	    when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-//	    when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
-//	    parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-//	} catch (Exception e) {
-//	    e.printStackTrace();
-//	    throw new RuntimeException("Failed to set up test mock objects");
-//	}
-//    }
-//
-//    @DisplayName("Incoming vehicule")
-//    @Test
-//    public void processIncomingVehicle() {
-//
-//    }
-//
-//    @DisplayName("Incoming vehicule (Regular client) notify 5% discount")
-//    @Test
-//    public void processIncomingVehicleForRegularClient() {
-//
-//    }
-//
-//    @DisplayName("Exiting vehicule")
-//    @Test
-//    public void processExitingVehicleTest() {
-//	parkingService.processExitingVehicle();
-//	verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
-//    }
-//
-//    @DisplayName("Exiting vehicule (Regular client) with 5% discount")
-//    @Test
-//    public void processExitingVehicleTestForRegularClient() {
-//
-//    }
+    @BeforeEach
+    private void setUpPerTest() {
+	inTime = LocalDateTime.now().minusHours(1).minusMinutes(1); // To recover a parking out-time of one hour
+	try {
+	    when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+	    when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+	    parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+	    parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+	    ticket = new Ticket();
+	    ticket.setInTime(inTime);
+	    ticket.setParkingSpot(parkingSpot);
+	    ticket.setVehicleRegNumber("ABCDEF");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new RuntimeException("Failed to set up test mock objects");
+	}
+    }
+
+    @Test
+    @Tag("Incoming")
+    @DisplayName("Incoming vehicle - new vehicle")
+    public void givenCarEntry_whenProcessIncomingVehicle_thenCheckGoodGenerationOfTicket() {
+	when(inputReaderUtil.readSelection()).thenReturn(1);
+	when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+
+	parkingService.processIncomingVehicle();
+
+	verify(inputReaderUtil, times(1)).readSelection();
+	verify(parkingSpotDAO, times(1)).getNextAvailableSlot(any(ParkingType.class));
+	verify(ticketDAO, times(1)).saveTicket(any(Ticket.class));
+	verify(parkingSpotDAO, times(1)).updateParking(any(ParkingSpot.class));
+	verify(ticketDAO, times(1)).getTicket(anyString());
+    }
+
+    @Test
+    @Tag("Exiting")
+    @DisplayName("Exiting vehicle - ParkingSpot updated")
+    public void givenParkedCar_whenExiting_thenParkingSpotMustBeUpdated() {
+	when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+	when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+
+	parkingService.processExitingVehicle();
+
+	verify(ticketDAO, times(1)).getTicket(anyString());
+	verify(ticketDAO, times(1)).checkNumberVisitsUser(anyString());
+	verify(ticketDAO, times(1)).updateTicket(any(Ticket.class));
+	verify(parkingSpotDAO, times(1)).updateParking(any(ParkingSpot.class));
+    }
 
 }
